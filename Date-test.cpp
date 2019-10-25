a326cd938f04a4f692615c4fd0bb70bcb3859d18
AChatard <alexandre.chatard@hotmail.fr>
2019-10-25 17:03:40 +0200

    Question 1b : Classe Date + choix de modification

--- Date-test.cpp
+++ Date-test.cpp
@@ -5,11 +5,39 @@
 
 using namespace std;
 
+
+
 int main(){
+
+int a=0, j=0, m=0, y=0;
+
 	Date anniv(1,10,1999);
 	cout << "Je suis né le " << anniv.getDay() << " " 
 	<< anniv.getMonth() << " " << anniv.getYear() <<
 	endl;
+
+cout << "voulez vous modifier la date ? (Appuyer sur 1)" << endl;
+cin >> a;
+
+if (a==1)
+ {
+ 	cout << "Entrez un jour" << endl;
+ 	cin >> j;
+
+ 	cout << "Entrez un mois" << endl;
+ 	cin >> m;
+
+ 	cout << "Entrez une année" << endl;
+ 	cin >> y;
+
+ 	Date NewDate(j,m,y);
+
+ 	cout << "la nouvelle date est " << NewDate.getDay() << " " 
+	<< NewDate.getMonth() << " " << NewDate.getYear() <<
+	endl;
+ } 
+
+
 
 return 0;
 }
